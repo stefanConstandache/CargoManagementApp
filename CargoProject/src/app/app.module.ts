88feddf2483c688f6468b347aaee681dccd92e02
stefanConstandache <stefan.m.constandache@gmail.com>
2022-01-05 23:53:42 +0200

    Created sign up, sign in and logout functionalities

--- CargoProject/src/app/app.module.ts
+++ CargoProject/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
+
+const config = {
+  apiKey: "AIzaSyA7luWGbMhIgXImE-ZnHWwWljs35kK6GRg",
+  authDomain: "cargo-management-4e50a.firebaseapp.com",
+  projectId: "cargo-management-4e50a",
+  storageBucket: "cargo-management-4e50a.appspot.com",
+  messagingSenderId: "1097151939401",
+  appId: "1:1097151939401:web:9a6e54511306e455825098",
+  measurementId: "G-BG7G8D3TDQ"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
